dca950e59b4f3773d86047e4736eebdc28b0553a
Javier de la Serna Martínez <37005341+Omega-Dejavi77@users.noreply.github.com>
2020-03-27 17:23:17 +0100

    Toolbar personalized

--- app/src/main/java/com/example/minitwitter/ui/DashboardActivity.java
+++ app/src/main/java/com/example/minitwitter/ui/DashboardActivity.java
@@ -1,6 +1,8 @@
 package com.example.minitwitter.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.minitwitter.R;
@@ -16,19 +18,34 @@ import androidx.navigation.ui.NavigationUI;
 
 public class DashboardActivity extends AppCompatActivity {
 
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = item -> {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+            case R.id.navigation_tweets_like:
+            case R.id.navigation_profile:
+                return true;
+        }
+        return false;
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        getSupportActionBar().hide();
+
+        /*BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_tweets_like, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);*/
+
+        BottomNavigationView navigationView = findViewById(R.id.navigation_home);
 
         String token = SharedPreferencesManager.readStringValue(Constants.PREF_TOKEN);
         Toast.makeText(this, token, Toast.LENGTH_SHORT).show();
